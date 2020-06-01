a08d08ccdab85644f459532dc1fa4c46910a017a
hi!there <getstarted0987@gmail.com>
2020-06-01 16:23:30 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by mMacbook Air on 2020/06/01.
+//  Copyright © 2020 mMacbook pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
